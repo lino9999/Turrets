d37c5ee1829af07284b8e1f549b42b53d6c64821
Lino <lino9999.123@gmail.com>
2025-07-27 19:48:34 +0200

    Initial commit

---
+++ src/main/java/com/Lino/turrets/Turrets.java
@@ -0,0 +1,17 @@
+package com.Lino.turrets;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Turrets extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
